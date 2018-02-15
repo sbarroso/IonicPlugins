781cb8365f2efc83c3e32f2dbcff40d8093b3466
Santiago Barroso <sbarroso.dev@gmail.com>
2018-02-15 20:37:59 +0100

    Added examples

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,8 +4,23 @@ import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 
+import { CallNumber } from '@ionic-native/call-number';
+import { AppVersion } from '@ionic-native/app-version';
+import { Brightness } from '@ionic-native/brightness';
+import { FingerprintAIO } from '@ionic-native/fingerprint-aio';
+import { Flashlight } from '@ionic-native/flashlight'
+import { SQLite } from '@ionic-native/sqlite';
+import { SpeechRecognition } from '@ionic-native/speech-recognition';
+import { Network } from '@ionic-native/network';
+import { Badge } from '@ionic-native/badge';
+import { Vibration } from '@ionic-native/vibration';
+import { VideoPlayer } from '@ionic-native/video-player';
+
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { DatabaseProvider } from '../providers/database/database';
+// import { AdvertProvider } from '../providers/advert/advert';
+// import { AdMob } from '@ionic-native/admob';
 
 @NgModule({
   declarations: [
@@ -24,7 +39,21 @@ import { HomePage } from '../pages/home/home';
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    CallNumber,
+    AppVersion,
+    // AdvertProvider,
+    // AdMob,
+    Brightness,
+    FingerprintAIO,
+    Flashlight,
+    SQLite,
+    DatabaseProvider,
+    SpeechRecognition,
+    Network,
+    Badge,
+    Vibration,
+    VideoPlayer
   ]
 })
 export class AppModule {}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -8,7 +8,11 @@ import { NavController } from 'ionic-angular';
 export class HomePage {
 
   constructor(public navCtrl: NavController) {
-
   }
+
+  showPage(name) {
+    this.navCtrl.push(name);
+  }
+
 
 }
